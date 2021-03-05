6afe7ed396638d69b01d9a1c6ed4d20a3421b51b
Huy Vo <HuyVo@my.unt.edu>
2021-03-05 17:04:10 -0600

    Add Sign-in and Sign-up form

--- FitTrek.py
+++ FitTrek.py
@@ -1,35 +1,80 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, flash
+from forms import SignInForm, SignUpForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '982b8f6e08e8cedff2c6deb24a40bbe6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), unique=True, default='avatar.png')
+    password = db.Column(db.String(60), nullable=False)
+    post_attribute = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.data_posted}')"
 
 @app.route("/")
 def index():
     return render_template('index copy.html')
 
+
 @app.route("/home")
 def home():
     return render_template('index copy.html')
+
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
+
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+
 
 @app.route("/post")
 def post():
     return render_template('post.html')
 
-@app.route("/signin")
-def login():
-    return render_template('signin.html')
 
-@app.route("/signup")
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.last_name.data}!', 'success')
+        return redirect(url_for('signin'))
+    return render_template('signup.html', title='Register', form=form)
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    form = SignInForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('Welcome!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('signin.html', form=form)
 
 
 if __name__ == "__main__":
---
+++ forms.py
@@ -0,0 +1,18 @@
+from wtforms import StringField, PasswordField, SubmitField, Form, validators, BooleanField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class SignUpForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired(), Length(min=2, max=20)])
+    last_name = StringField('Last name', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class SignInForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
